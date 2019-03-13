0addedbc6ba8add243716f5b05a4b3f71ca5a9e8
Krak <adada@gmail.com>
2019-03-13 01:19:23 +0100

    added meta_install variable to install metasploit-framework

--- updater.py
+++ updater.py
@@ -6,6 +6,9 @@ su = "sudo apt "
 sui = "sudo apt install -y "
 docker_net = "sudo docker network create --subnet 192.168.0.0/16 --gateway 192.168.0.1 --ip-range=192.168.0.0/24 --driver=bridge bridge5"
 docker_pull = "docker pull track2name/ubuntu && docker pull ubuntu"
+meta_install = "curl https://raw.githubusercontent.com/rapid7/metasploit-omnibus/master/config/templates/metasploit-framework-wrappers/msfupdate.erb > msfinstall && \
+chmod 755 msfinstall && \
+./msfinstall"
 
 def update():
     os.system(su + 'update')
