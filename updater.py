9220938a1d3c2ccb6a06902bf162ad707c82da6e
Krak <adada@gmail.com>
2019-03-14 23:51:48 +0100

    added add docker repository to docker_repo variable

--- updater.py
+++ updater.py
@@ -6,6 +6,8 @@ su = "sudo apt "
 sui = "sudo apt install -y "
 
 docker_net = "sudo docker network create --subnet 192.168.0.0/16 --gateway 192.168.0.1 --ip-range=192.168.0.0/24 --driver=bridge bridge5"
+docker_repo = 'sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"'
+
 docker_pull = "docker pull track2name/ubuntu && docker pull ubuntu"
 meta_install = "curl https://raw.githubusercontent.com/rapid7/metasploit-omnibus/master/config/templates/metasploit-framework-wrappers/msfupdate.erb > msfinstall && \
 chmod 755 msfinstall && ./msfinstall"
@@ -29,10 +31,6 @@ def apt_install():
     os.system(sui + 'leafpad')
     os.system(sui + 'net-tools')
     os.system(sui + 'ssh')
-    os.system(sui + 'keepass2')
-    os.system(sui + 'lua5.3')
-    os.system(sui + 'gitk')
-    os.system(sui + 'gparted')
     os.system(sui + 'ifupdown')
     os.system(sui + 'gnome-tweak-tool')
 
@@ -47,10 +45,7 @@ def docker_install():
     os.system('curl -fsSL https://download.docker.com/linux/debian/gpg  | sudo apt-key add -')
     os.system('sudo apt-key fingerprint 0EBFCD88')
 
-    os.system('sudo add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"')
+    os.system(docker_repo)
 
     os.system(su + 'update')
     os.system(su + 'docker-ce docker-ce-cli containerd.io')
