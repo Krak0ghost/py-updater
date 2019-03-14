5bfa9bdb0514d368103dd842842f7a83fe7d4f6b
Krak <adada@gmail.com>
2019-03-14 23:36:01 +0100

    added update on add_repository func

--- updater.py
+++ updater.py
@@ -18,7 +18,9 @@ def update():
     os.system(su + 'dist-upgrade -y')
 
 def add_repository():
+    
     os.system('sudo add-apt-repository ppa:webupd8team/y-ppa-manager')
+    os.system(su + 'update')
 
 def apt_install():
     os.system(sui + 'vim')
